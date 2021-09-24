2a386c8cf25cfb2e616ee734bc33eb14081f9626
Bima Bagaskhoro <bimabagas644@gmail.com>
2021-09-24 23:47:54 +0700

    second update

--- app/src/main/java/com/bimabagaskhoro/uigitstugas18/model/ResponseStatus.kt
+++ app/src/main/java/com/bimabagaskhoro/uigitstugas18/model/ResponseStatus.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class ResponseSukses(
+data class ResponseInsert(
 
 	@field:SerializedName("message")
 	val message: String? = null,
